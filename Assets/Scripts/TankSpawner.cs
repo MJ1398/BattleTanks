53c10380d5b5050e4626fba4c584161e0964ea28
Vikrant Kataria <vik7am@gmail.com>
2022-04-13 15:37:31 +0530

    spawn tank using MVC

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class TankSpawner : MonoBehaviour
 {
-    public GameObject tankPrefab;
-    // Start is called before the first frame update
+    public TankView tankView;
+
     void Start()
     {
-        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+        CreateTank();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void CreateTank(){
+        TankModel tankModel = new TankModel();
+        TankController tankController = new TankController(tankModel, tankView);
     }
 }
